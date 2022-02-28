03a549a28aa3e56c0af092d3cb4793fee6d1397b
darsay8 <darsay.one@gmail.com>
2022-02-28 12:45:19 -0300

    sass and router dom added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import JournalApp from './JournalApp';
 
+import './styles/styles.scss';
+
 const root = document.getElementById('root');
 render(<JournalApp />, root);
