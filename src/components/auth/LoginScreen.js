dfac3a91ad6017efbf0cb7af2df1d187e7675c84
darsay8 <darsay.one@gmail.com>
2022-02-28 16:15:59 -0300

    login/register screens and styles added

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginScreen = () => {
   return (
     <div>
-      <h1>LoginScreen</h1>
+      <h3 className="auth__title">Login</h3>
+      <form className="animate__animated animate__fadeIn animate__faster">
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          autoComplete="off"
+          className="auth__input"
+        />
+        <input type="password" placeholder="Password" name="password" className="auth__input" />
+        <button type="submit" className="btn btn-primary btn-block">
+          Login
+        </button>
+
+        <div className="auth__social-networks">
+          <p>Login with social networks</p>
+          <div className="google-btn">
+            <div className="google-icon-wrapper">
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="google button"
+              />
+            </div>
+            <p className="btn-text">
+              <b>Sign in with google</b>
+            </p>
+          </div>
+        </div>
+        <div className="mt-1">
+          <Link to="/auth/register" className="link">
+            Create new account
+          </Link>
+        </div>
+      </form>
     </div>
   );
 };
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const RegisterScreen = () => {
   return (
     <div>
-      <h1>RegisterScreen</h1>
+      <h3 className="auth__title">Register</h3>
+      <form className="animate__animated animate__fadeIn animate__faster">
+        <input
+          type="name"
+          placeholder="Name"
+          name="name"
+          autoComplete="off"
+          className="auth__input"
+        />
+
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          autoComplete="off"
+          className="auth__input"
+        />
+
+        <input type="password" placeholder="Password" name="password" className="auth__input" />
+
+        <input
+          type="password"
+          placeholder="Confirm password"
+          name="password2"
+          className="auth__input"
+        />
+
+        <button type="submit" className="btn btn-primary btn-block mb-5">
+          Register
+        </button>
+
+        <div className="mt-1">
+          <Link to="/auth/login" className="link">
+            Already Registered?
+          </Link>
+        </div>
+      </form>
     </div>
   );
 };
