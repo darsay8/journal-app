53f72cd4b8fb903626724d898a79045249966006
darsay8 <darsay.one@gmail.com>
2022-03-02 15:53:50 -0300

    action and handle form added

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import { login } from '../../actions/auth';
 
 const LoginScreen = () => {
+  const dispatch = useDispatch();
+
+  const [formValues, handleInputChange] = useForm({ email: 'user@mail.com', password: '123' });
+
+  const { email, password } = formValues;
+
+  const handleLogin = e => {
+    e.preventDefault();
+    dispatch(login(123, 'User'));
+  };
+
   return (
     <div>
       <h3 className="auth__title">Login</h3>
-      <form className="animate__animated animate__fadeIn animate__faster">
+      <form onSubmit={handleLogin} className="animate__animated animate__fadeIn animate__faster">
         <input
           type="email"
           placeholder="Email"
           name="email"
           autoComplete="off"
           className="auth__input"
+          value={email}
+          onChange={handleInputChange}
         />
-        <input type="password" placeholder="Password" name="password" className="auth__input" />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          className="auth__input"
+          value={password}
+          onChange={handleInputChange}
+        />
         <button type="submit" className="btn btn-primary btn-block">
           Login
         </button>
