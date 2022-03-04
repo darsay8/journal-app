68a5e86107103bf13725034baf36af4f1b251ac3
darsay8 <darsay.one@gmail.com>
2022-03-04 10:14:20 -0300

    keep auth state

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import JournalScreen from '../components/journal/JournalScreen';
 import AuthRouter from './AuthRouter';
+import { login } from '../actions/auth';
 
 const AppRouter = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const auth = getAuth();
+
+    onAuthStateChanged(auth, user => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+      }
+    });
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
