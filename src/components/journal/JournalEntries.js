6191a0f46b443d486645cbd5b651e46ef5e3cefd
darsay8 <darsay.one@gmail.com>
2022-03-04 20:05:20 -0300

    display notes, moment js added

--- src/components/journal/JournalEntries.js
+++ src/components/journal/JournalEntries.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import JournalEntry from './JournalEntry';
 
-const entries = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 const JournalEntries = () => {
+  const { notes } = useSelector(state => state.notes);
+
   return (
     <div className="journal__entries animate__animated animate__fadeIn animate__faster">
-      {entries.map(value => (
-        <JournalEntry key={value} />
+      {notes.map(note => (
+        <JournalEntry key={note.id} {...note} />
       ))}
     </div>
   );
--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,26 +1,27 @@
-const JournalEntry = () => {
-  const imgUrl =
-    'https://geniusbeauty.com/wp-content/uploads/2008/11/california-wallpaper-nature-10.jpg';
+import moment from 'moment';
+
+const JournalEntry = ({ id, date, title, body, url }) => {
+  const noteDate = moment(date);
 
   return (
     <div className="journal__entry animate__animated animate__fadeIn animate__faster">
-      <div
-        className="journal__entry-picture"
-        style={{
-          backgroundSize: 'cover',
-          backgroundImage: `url(${imgUrl})`,
-        }}
-      ></div>
+      {url && (
+        <div
+          className="journal__entry-picture"
+          style={{
+            backgroundSize: 'cover',
+            backgroundImage: `url(${url})`,
+          }}
+        ></div>
+      )}
 
       <div className="journal__entry-body">
-        <p className="journal__entry-title">Entry</p>
-        <p className="journal__entry-content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
+        <p className="journal__entry-title">{title}</p>
+        <p className="journal__entry-content">{body}</p>
       </div>
       <div className="journal__entry-date-box">
-        <span>Monday</span>
-        <h4>28</h4>
+        <span>{noteDate.format('dddd')}</span>
+        <h4>{noteDate.format('Do')}</h4>
       </div>
     </div>
   );
